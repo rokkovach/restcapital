08ea1e7b30a1be31ac2531149efd6008d1cb9ae9
Rok Kovac <rok-kovac@hotmail.com>
2021-01-29 19:14:01 +0100

    fixing JSX

--- pages/index.js
+++ pages/index.js
@@ -5,21 +5,21 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>RestCaiptal - Future of finance is here</title>
+        <title>RestCapital - Future of Finance is here</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <span style="font-family: Impact">Rest<span class="color:red">Capital</span></span>
+          <span>Welcome to <span style={{fontFamily: "Impact"}}>Rest<span style={{color: "red"}}>Capital</span></span></span>
         </h1>
-        <p>We are the leading startup Hedge fund capital. Focusing on most promising ALT coins and meme stocks.</p>
-        <p>Our main philosophy is that stock can only go up, so why don't invest all your money with us?</p>
-        <p>Disclaimer: not beholden to any Wall Street suit, just to <a href="https://www.reddit.com/r/wallstreetbets/" target="_BLANK">wallstreetbets</a></p>
+        <p style={{textAlign: "center"}}>We are the leading startup Hedge fund capital. Focusing on most promising ALT coins and meme stocks.<br />
+        Our main philosophy is that stock can only go up, so why don't invest all your money with us?<br />
+        Disclaimer: not beholden to any Wall Street suit, just to <a style={{color: "red"}} href="https://www.reddit.com/r/wallstreetbets/" target="_BLANK">wallstreetbets</a></p>
       </main>
 
       <footer className={styles.footer}>
-          Powered by <i>Stupidity</i>
+          <i>Powered by Stupidity</i>
       </footer>
     </div>
   )
